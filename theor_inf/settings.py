d5fcb673c6cfcff3f35a70ba847e6f45a6b01b5c
YarGorbunov <yaroslav-gorbunov99@mail.ru>
2024-12-26 20:17:20 +0300

    <3

--- theor_inf/settings.py
+++ theor_inf/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@iw+b72*1rmn2qbin^lean(ri)h#jr#m05ebxzn$^$dw^ejms_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
